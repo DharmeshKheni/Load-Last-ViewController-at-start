b889adf2f3f4e354b6c1e0a9cc4d17dbe64c1407
Dharmesh <khenid21@gmail.com>
2015-06-11 12:26:59 +0530

    Load Last ViewController At Start
    Load Last ViewController At Start

---
+++ testing1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testing1
+//
+//  Created by Anil on 11/06/15.
+//  Copyright (c) 2015 Variya Soft Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSUserDefaults.standardUserDefaults().setInteger(0, forKey: "View")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
